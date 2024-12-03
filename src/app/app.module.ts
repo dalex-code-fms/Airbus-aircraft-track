db92ecb19bd7f903fda3a4baf4f53fa3fa17dac7
Ferreira-RodriguesD <david.ferreira-rodrigues@fms-ea.com>
2024-12-03 09:56:11 +0100

    generated home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { Aircrafts } from './model/aircrafts';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
