5ed4b1151fa466e6701fb261b62cde8d7466dc73
NathPascal <nath.pascal03@gmail.com>
2024-12-03 13:34:27 +0100

    modif aircrafts.model

--- src/app/model/aircrafts.ts
+++ src/app/model/aircrafts.ts
@@ -1,12 +1,10 @@
 import { AicraftParts } from './aicraft-parts';
 
-export class Aircrafts {
-  constructor(
-    public id: number,
-    public name: string,
-    public type: string,
-    public phase: string,
-    public equipments: AicraftParts[],
-    public image: string
-  ) {}
+export interface Aircrafts {
+  id: number,
+  name: string,
+  type: string,
+  phase: string,
+  equipments: AicraftParts[],
+  image: string
 }
